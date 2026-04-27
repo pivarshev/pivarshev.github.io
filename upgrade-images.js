693316a145cee553716a5ea12bd6b77eb5a0a40c
Shailen Parmar <shailenparmar@gmail.com>
2026-04-27 10:07:16 -0500

    Upgrade images as they scroll into view, not after full page load

--- upgrade-images.js
+++ upgrade-images.js
@@ -1,18 +1,23 @@
 // Progressive image loading: swap small thumbnails for high-res versions.
 // Images start at ~400px (artworks_small/) for fast load,
-// then upgrade to ~2000px (artworks/) in the background.
-window.addEventListener('load', function () {
-    var images = document.querySelectorAll('img[src*="artworks_small/"]');
-    var delay = 50;
+// then upgrade to ~2000px (artworks/) as they come into view.
+(function () {
+    var observer = new IntersectionObserver(function (entries) {
+        entries.forEach(function (entry) {
+            if (entry.isIntersecting) {
+                var img = entry.target;
+                var hiSrc = img.src.replace('artworks_small/', 'artworks/');
+                var hiRes = new Image();
+                hiRes.onload = function () {
+                    img.src = hiSrc;
+                };
+                hiRes.src = hiSrc;
+                observer.unobserve(img);
+            }
+        });
+    }, { rootMargin: '200px' });
 
-    images.forEach(function (img, i) {
-        setTimeout(function () {
-            var hiRes = new Image();
-            var hiSrc = img.src.replace('artworks_small/', 'artworks/');
-            hiRes.onload = function () {
-                img.src = hiSrc;
-            };
-            hiRes.src = hiSrc;
-        }, delay * i);
+    document.querySelectorAll('img[src*="artworks_small/"]').forEach(function (img) {
+        observer.observe(img);
     });
-});
+})();
